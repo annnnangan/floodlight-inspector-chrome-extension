8ac8ca6e6c2a0f0750f2a782150eaaf88d1b9485
anna <anna.ymngan@gmail.com>
2024-11-04 22:24:52 +0800

    render Floodlight info to event data section

--- panel.js
+++ panel.js
@@ -1,10 +1,8 @@
-const result = document.querySelector(".result");
 const accordionFloodlight = document.querySelector("#accordion-floodlight");
-result.innerHTML = "";
 
-const uid = function () {
-  return Date.now().toString(36) + Math.random().toString(36).substr(2);
-};
+function uid() {
+  return Date.now().toString(36) + Math.random().toString(36);
+}
 
 chrome.devtools.network.onRequestFinished.addListener(function (request) {
   // Check if the URL includes "fls.doubleclick"
@@ -18,9 +16,11 @@ chrome.devtools.network.onRequestFinished.addListener(function (request) {
       src: requestURL.split(";")[1],
       type: requestURL.split(";")[2],
       cat: requestURL.split(";")[3],
+      ord: requestURL.split(";")[4],
     };
     let identifier = uid();
     const requestURAccordion = fullRequestURL(requestURL);
+    const eventDataAccordion = renderFloodlightInfo(floodlight, requestURL);
     accordionFloodlight.innerHTML += `<div class="accordion-item">
         <h2 class="accordion-header">
           <button
@@ -43,6 +43,7 @@ chrome.devtools.network.onRequestFinished.addListener(function (request) {
           data-bs-parent="#accordion-floodlight"
         >
           <div class="accordion-body">
+            ${eventDataAccordion}
             ${requestURAccordion}
           </div>
         </div>
@@ -65,4 +66,68 @@ function fullRequestURL(requestURL) {
     </div>
   </div>
 </div>`;
+}
+
+function renderFloodlightInfo(floodlight, requestURL) {
+  const customFloodlightVariable = renderCustomFloodlightVariable(requestURL);
+  return ` <div class="accordion mb-4" id="accordion-floodlight-details">
+              <div class="accordion-item">
+                <h2 class="accordion-header">
+                  <button
+                    class="accordion-button"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapseOne"
+                    aria-expanded="true"
+                    aria-controls="collapseOne"
+                  >
+                    Event Data
+                  </button>
+                </h2>
+                <div
+                  id="collapseOne"
+                  class="accordion-collapse collapse show"
+                  data-bs-parent="#accordion-full-request-url"
+                >
+                  <div class="accordion-body">
+                    <ul>
+                      <li class="row">
+                        <p class="col-6">Advertiser ID(src)</p>
+                        <p class="col-6">${floodlight.src}</p>
+                      </li>
+                      <li class="row">
+                        <p class="col-6">Activity Type String(cat)</p>
+                        <p class="col-6">${floodlight.cat}</p>
+                      </li>
+                      <li class="row">
+                        <p class="col-6">Group Tag String(type)</p>
+                        <p class="col-6">${floodlight.type}</p>
+                      </li>
+                      <li class="row">
+                        <p class="col-6">Counting Method</p>
+                        <p class="col-6">${floodlight.ord}</p>
+                      </li>
+                      ${customFloodlightVariable}
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>`;
+}
+
+function renderCustomFloodlightVariable(requestURL) {
+  let customFloodlightVariable = [];
+  let cfvLayout = "";
+  requestURL.split(";").filter((item) => {
+    item.match(/u[0-9]{1,}=.*/g) && customFloodlightVariable.push(item);
+  });
+
+  for (let variable of customFloodlightVariable) {
+    cfvLayout += `<li class="row">
+      <p class="col-6">${variable.split("=")[0]}</p>
+      <p class="col-6">${decodeURIComponent(variable.split("=")[1])}</p>
+    </li>`;
+  }
+
+  return cfvLayout;
 }
