ee954bf267462c43e4d674f0b038c405108df415
annnnangan <anna.ymngan@gmail.com>
2024-11-13 12:20:34 +0800

    Refactor panel js

--- panel.js
+++ panel.js
@@ -1,131 +1,208 @@
-const accordionFloodlight = document.querySelector("#accordion-floodlight");
-
-function uid() {
-  return Date.now().toString(36) + Math.random().toString(36);
-}
+const floodlightContainer = document.querySelector(".floodlight-list");
 
 chrome.devtools.network.onRequestFinished.addListener(function (request) {
   // Check if the URL includes "fls.doubleclick"
 
   if (request.request.url.includes("fls.doubleclick") && request.response.status == 200) {
     const requestURL = request.request.url;
-    const floodlight = {
-      src: requestURL.split(";")[1],
-      type: requestURL.split(";")[2],
-      cat: requestURL.split(";")[3],
-      ord: requestURL.split(";")[4],
-    };
-    let identifier = uid();
-    const requestURAccordion = fullRequestURL(requestURL);
-    const eventDataAccordion = renderFloodlightInfo(floodlight, requestURL);
-    accordionFloodlight.innerHTML += `<div class="accordion-item">
-        <h2 class="accordion-header">
-          <button
-            class="accordion-button"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#collapse-${identifier}"
-            aria-expanded="true"
-            aria-controls="collapse-${identifier}"
-          >
-            <span class="text-bold">Floodlight</span>
-            <span class="badge text-bg-primary mx-2">${floodlight.src}</span>
-            <span class="badge text-bg-success me-2">${floodlight.type}</span>
-            <span class="badge text-bg-info">${floodlight.cat}</span>
-          </button>
-        </h2>
-        <div
-          id="collapse-${identifier}"
-          class="accordion-collapse collapse"
-          data-bs-parent="#accordion-floodlight"
-        >
-          <div class="accordion-body">
-            ${eventDataAccordion}
-            ${requestURAccordion}
+    const floodlightInfoList = generateFloodlightInfoList(requestURL);
+    const identifier = generateUID();
+    const timestamp = new Date().toISOString().split("T")[0] + " " + new Date().toLocaleTimeString();
+
+    const eventDataAccordion = renderEventDataAccordion(floodlightInfoList, identifier);
+    const requestURLAccordion = renderFullRequestURLAccordion(requestURL, identifier);
+    floodlightContainer.innerHTML += `      <div class="floodlight-wrapper">
+        <div class="timestamp mb-1">
+          <p class="mb-0">Timestamp: <span>${timestamp}</span></p>
+        </div>
+
+        <div class="accordion mb-4 accordion-floodlight" id="accordionFloodlight-${identifier}">
+          <div class="accordion-item">
+            <!-- wrapper header -->
+            <h2 class="accordion-header">
+              <button
+                class="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseFloodlight-${identifier}"
+                aria-expanded="false"
+                aria-controls="collapseFloodlight-${identifier}">
+                <!-- wrapper header - content -->
+                <div class="flex-wrap">
+                  <span class="fw-bold me-2 mb-1">Floodlight</span>
+                  <span class="badge text-bg-primary mb-1">src=${floodlightInfoList.src}</span>
+                  <span class="badge text-bg-primary mb-1">type=${floodlightInfoList.type}</span>
+                  <span class="badge text-bg-primary mb-1">cat=${floodlightInfoList.cat}</span>
+                </div>
+              </button>
+            </h2>
+            <div id="collapseFloodlight-${identifier}" class="accordion-collapse collapse" data-bs-parent="#accordionFloodlight-${identifier}">
+              <!-- wrapper body -->
+              <div class="accordion-body">
+                ${eventDataAccordion}
+                ${requestURLAccordion}
+            </div>
           </div>
         </div>
       </div>`;
   }
 });
 
-function renderFloodlightInfo(floodlight, requestURL) {
-  const customFloodlightVariable = renderCustomFloodlightVariable(requestURL);
-  return ` <div class="accordion mb-4" id="accordion-floodlight-details">
-              <div class="accordion-item">
-                <h2 class="accordion-header">
-                  <button
-                    class="accordion-button"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#collapseOne"
-                    aria-expanded="true"
-                    aria-controls="collapseOne"
-                  >
-                    Event Data
-                  </button>
-                </h2>
-                <div
-                  id="collapseOne"
-                  class="accordion-collapse collapse show"
-                  data-bs-parent="#accordion-full-request-url"
-                >
-                  <div class="accordion-body">
-                    <ul>
-                      <li class="row">
-                        <p class="col-6">Advertiser ID(src)</p>
-                        <p class="col-6">${floodlight.src}</p>
-                      </li>
-                      <li class="row">
-                        <p class="col-6">Activity Type String(cat)</p>
-                        <p class="col-6">${floodlight.cat}</p>
-                      </li>
-                      <li class="row">
-                        <p class="col-6">Group Tag String(type)</p>
-                        <p class="col-6">${floodlight.type}</p>
-                      </li>
-                      <li class="row">
-                        <p class="col-6">Counting Method</p>
-                        <p class="col-6">${floodlight.ord}</p>
-                      </li>
-                      ${customFloodlightVariable}
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </div>`;
+function generateUID() {
+  return Date.now().toString(36) + Math.random().toString(36).substring(2);
 }
 
-function renderCustomFloodlightVariable(requestURL) {
+function generateFloodlightInfoList(requestURL) {
+  const requestURLParamArray = requestURL.split(";");
+  let floodlightDetails = {};
   let customFloodlightVariable = [];
-  let cfvLayout = "";
-  requestURL.split(";").filter((item) => {
+  let floodlightParams = ["src", "type", "cat", "ord", "num", "qty", "cost"];
+  for (let i = 0; i < floodlightParams.length; i++) {
+    floodlightDetails[floodlightParams[i]] = requestURLParamArray
+      .filter((item) => item.includes(`${floodlightParams[i]}=`))
+      .map((item) => item.split("=")[1])[0];
+  }
+
+  requestURLParamArray.filter((item) => {
     item.match(/u[0-9]{1,}=.*/g) && customFloodlightVariable.push(item);
   });
 
-  for (let variable of customFloodlightVariable) {
+  floodlightDetails["cfv"] = customFloodlightVariable;
+  floodlightDetails["countingMethod"] = defineCountingMethod(floodlightDetails);
+
+  return floodlightDetails;
+}
+
+function defineCountingMethod(floodlightInfoList) {
+  if (floodlightInfoList.ord && !floodlightInfoList.num && floodlightInfoList.qty !== undefined && floodlightInfoList.cost !== undefined) {
+    return "Transactions / Items Sold";
+  } else if (floodlightInfoList.ord == "1" && floodlightInfoList.num && !floodlightInfoList.qty && !floodlightInfoList.cost) {
+    return "Unique";
+  } else if (floodlightInfoList.ord && !floodlightInfoList.num && !floodlightInfoList.qty && !floodlightInfoList.cost) {
+    return "Standard / Session";
+  } else {
+    return "Unable to define a correct counting method";
+  }
+}
+
+function renderEventDataAccordion(floodlightInfoList, identifier) {
+  const customFloodlightVariableLayout = renderCustomFloodlightVariable(floodlightInfoList);
+  return `<div class="accordion mb-4 accordion-event-data" id="accordionEventData-${identifier}">
+                  <div class="accordion-item">
+                    <h2 class="accordion-header">
+                      <button
+                        class="accordion-button"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#collapseEventData-${identifier}"
+                        aria-expanded="false"
+                        aria-controls="collapseEventData-${identifier}">
+                        Event Data
+                      </button>
+                    </h2>
+                    <div
+                      id="collapseEventData-${identifier}"
+                      class="accordion-collapse collapse"
+                      data-bs-parent="#accordionEventData-${identifier}">
+                      <div class="accordion-body">
+                      <div>
+                          <p class="fw-bold text-primary">Identify your tag with below identifier</p>
+                          <ul>
+                            <li class="row">
+                              <p class="col-6">Advertiser ID(src)</p>
+                              <p class="col-6">${floodlightInfoList.src}</p>
+                            </li>
+                            <li class="row">
+                              <p class="col-6">Activity Type String(cat)</p>
+                              <p class="col-6">${floodlightInfoList.cat}</p>
+                            </li>
+                            <li class="row">
+                              <p class="col-6">Group Tag String(type)</p>
+                              <p class="col-6">${floodlightInfoList.type}</p>
+                            </li>
+                          </ul>
+                        </div>
+                        <div>
+                          <p class="fw-bold text-primary">Confirm your tag counting method</p>
+                          <ul>
+                            <li class="row">
+                              <p class="col-6">Counting Method</p>
+                              <p class="col-6">${floodlightInfoList.countingMethod}</p>
+                            </li>
+                            <li class="row">
+                              <p class="col-6">ord</p>
+                              <p class="col-6">${floodlightInfoList.ord}</p>
+                            </li>
+                            <li class="row">
+                              <p class="col-6">num</p>
+                              <p class="col-6">${floodlightInfoList.num}</p>
+                            </li>
+                            <li class="row">
+                              <p class="col-6">qty</p>
+                              <p class="col-6">${floodlightInfoList.qty}</p>
+                            </li>
+                            <li class="row">
+                              <p class="col-6">cost</p>
+                              <p class="col-6">${floodlightInfoList.cost}</p>
+                            </li>
+                          </ul>
+                        </div>
+                        <div>
+                          <p class="fw-bold text-primary">Check if custom Floodlight variables are passing expected values</p>
+                            ${customFloodlightVariableLayout}
+                        </div>
+                     
+                      </div>
+                    </div>
+                  </div>
+                </div>`;
+}
+
+function renderCustomFloodlightVariable(floodlightInfoList) {
+  if (floodlightInfoList.cfv.length == 0) {
+    return "<p>- No custom floodlight variable -</p>";
+  }
+
+  let cfvLayout = "";
+
+  let cfvList = floodlightInfoList.cfv;
+
+  for (let variable of cfvList) {
     cfvLayout += `<li class="row">
       <p class="col-6">${variable.split("=")[0]}</p>
       <p class="col-6">${decodeURIComponent(variable.split("=")[1])}</p>
     </li>`;
   }
 
-  return cfvLayout;
+  let finalCFVLayout = "<ul>" + cfvLayout + "</ul>";
+
+  return finalCFVLayout;
 }
 
-//show full request url to the screen
-function fullRequestURL(requestURL) {
-  return `<div class="accordion" id="accordion-full-request-url">
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        Full Request URL
-      </button>
-    </h2>
-    <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordion-full-request-url">
-      <div class="accordion-body text-break">
-        ${requestURL}
-      </div>
-    </div>
-  </div>
-</div>`;
+function renderFullRequestURLAccordion(requestURL, identifier) {
+  return `                <div class="accordion accordion-full-request-url" id="accordionFullRequestURL-${identifier}">
+                  <div class="accordion-item">
+                    <h2 class="accordion-header">
+                      <button
+                        class="accordion-button"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#collapseFullRequestURL-${identifier}"
+                        aria-expanded="false"
+                        aria-controls="collapseFullRequestURL-${identifier}">
+                        Full Request URL
+                      </button>
+                    </h2>
+                    <div
+                      id="collapseFullRequestURL-${identifier}"
+                      class="accordion-collapse collapse"
+                      data-bs-parent="#accordionFullRequestURL-${identifier}">
+                      <div class="accordion-body">
+                        <p>
+                         ${requestURL}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>`;
 }
--- test.js
+++ test.js
@@ -1,12 +1,74 @@
-const test =
-  "https://9811262.fls.doubleclick.net/activityi;src=9811262;type=gener0;cat=gener0;ord=5839327051755;npa=0;auiddc=1624618298.1730531660;u1=en;u11=https%3A%2F%2Ffivestones.net%2F;gdid=dMWZhNz;ps=1;pcor=1411101188;uaa=;uab=64;uafvl=Chromium%3B130.0.6723.59%7CGoogle%2520Chrome%3B130.0.6723.59%7CNot%253FA_Brand%3B99.0.0.0;uamb=1;uam=Nexus%205;uap=Android;uapv=6.0;uaw=0;frm=0;gtm=45fe4au0v9182868030z8812964584za201zb812964584;gcs=G111;gcd=13r3r3r3r5l1;dma=0;tag_exp=101823848~101878899~101878944~101925629;epver=2;~oref=https%3A%2F%2Ffivestones.net%2F?";
-
-// console.log(test.split(".")[0].split("/")[2]);
-console.log(test.split(";")[1]);
-console.log(test.split(";")[2]);
-
 // const uid = function () {
-//   return Date.now().toString(36) + Math.random().toString(36).substr(2);
-// };
+//   return Date.now().toString(36) + Math.random().toString(36).substring(2);
+// }; //m3ea9ceel4tt571w8uj
 
 // console.log(uid());
+
+const transactionRequestURL =
+  "https://12345678.fls.doubleclick.net/activityi;src=12345678;type=purc;cat=purc01;qty=1;cost=3400;ord=HK20241112022;npa=0;auiddc=2088102233.1731384606;u5=Sunglasses%20%E5%A4%AA%E9%99%BD%E7%9C%BC%E9%8F%A1%7CWatch%20%E6%89%8B%E9%8C%B6;u6=HKS20432%7CHKW93852;u7=1200%7C2200;u8=1%7C1;u9=HK20241112022;u10=3400;ps=1;pcor=1302370007;uaa=arm;uab=64;uafvl=Not%252FA)Brand%3B8.0.0.0%7CChromium%3B126.0.6478.62%7CGoogle%2520Chrome%3B126.0.6478.62;uamb=0;uam=;uap=macOS;uapv=14.5.0;uaw=0;pscdl=noapi;frm=0;gtm=45fe4b70z89199698002za201zb9199698002;gcd=13l3l3l3l1l1;dma=0;tag_exp=101823848~101925629~102067554~102077854;epver=2;~oref=https%3A%2F%2Fannnnangan.github.io%2Fdemo-page-for-floodlight-inspector-chrome-extension%2F%3Fgtm_debug%3D1731385548856?";
+const itemSoldRequestURL =
+  "https://12345678.fls.doubleclick.net/activityi;src=12345678;type=purc;cat=purc02;qty=2;cost=3400;ord=HK20241112022;npa=0;auiddc=2088102233.1731384606;u5=Sunglasses%20%E5%A4%AA%E9%99%BD%E7%9C%BC%E9%8F%A1%7CWatch%20%E6%89%8B%E9%8C%B6;u6=HKS20432%7CHKW93852;u7=1200%7C2200;u8=1%7C1;u9=HK20241112022;u10=3400;ps=1;pcor=998479116;uaa=arm;uab=64;uafvl=Not%252FA)Brand%3B8.0.0.0%7CChromium%3B126.0.6478.62%7CGoogle%2520Chrome%3B126.0.6478.62;uamb=0;uam=;uap=macOS;uapv=14.5.0;uaw=0;pscdl=noapi;frm=0;gtm=45fe4b70z89199698002za201zb9199698002;gcd=13l3l3l3l1l1;dma=0;tag_exp=101823848~101925629~102067554~102077854;epver=2;~oref=https%3A%2F%2Fannnnangan.github.io%2Fdemo-page-for-floodlight-inspector-chrome-extension%2F%3Fgtm_debug%3D1731385548856?";
+
+const uniqueRequestURL =
+  "https://12345678.fls.doubleclick.net/activityi;src=12345678;type=consi;cat=contact;ord=1;num=7943641807276;npa=0;auiddc=2088102233.1731384606;ps=1;pcor=47659789;uaa=arm;uab=64;uafvl=Not%252FA)Brand%3B8.0.0.0%7CChromium%3B126.0.6478.62%7CGoogle%2520Chrome%3B126.0.6478.62;uamb=0;uam=;uap=macOS;uapv=14.5.0;uaw=0;pscdl=noapi;frm=0;gtm=45fe4b70z89199698002za201zb9199698002;gcd=13l3l3l3l1l1;dma=0;tag_exp=101823848~101925629~102067554~102077854;epver=2;~oref=https%3A%2F%2Fannnnangan.github.io%2Fdemo-page-for-floodlight-inspector-chrome-extension%2F%3Fgtm_debug%3D1731385548856?";
+
+const standardRequestURL =
+  "https://12345678.fls.doubleclick.net/activityi;src=12345678;type=consi;cat=blog;ord=6838921844843;npa=0;auiddc=2088102233.1731384606;u1=View%20More;u2=https%3A%2F%2Fsupport.google.com%2Fcampaignmanager%2Fanswer%2F3027419%3Fhl%3Den;u3=What%20is%20Floodlight%20tag%3F;u4=Floodlight%20is%20used%20for%20conversion%20and%20event%20tracking%20across%20Google%20Marketing%20Platform%2C%20including%20Campaign%20Manager%20360%2C%20Display%20%26%20Video%20360%2C%20and%20the%20new%20Search%20Ads%20360.;ps=1;pcor=881917410;uaa=arm;uab=64;uafvl=Not%252FA)Brand%3B8.0.0.0%7CChromium%3B126.0.6478.62%7CGoogle%2520Chrome%3B126.0.6478.62;uamb=0;uam=;uap=macOS;uapv=14.5.0;uaw=0;pscdl=noapi;frm=0;gtm=45fe4b70z89199698002za201zb9199698002;gcd=13l3l3l3l1l1;dma=0;tag_exp=101823848~101925629~102067554~102077854;epver=2;~oref=https%3A%2F%2Fannnnangan.github.io%2Fdemo-page-for-floodlight-inspector-chrome-extension%2F%3Fgtm_debug%3D1731385548856?";
+
+function generateFloodlightInfoList(requestURL) {
+  const requestURLParamArray = requestURL.split(";");
+  let floodlightDetails = {};
+  let customFloodlightVariable = [];
+  let floodlightParams = ["src", "type", "cat", "ord", "num", "qty", "cost"];
+  for (let i = 0; i < floodlightParams.length; i++) {
+    floodlightDetails[floodlightParams[i]] = requestURLParamArray
+      .filter((item) => item.includes(`${floodlightParams[i]}=`))
+      .map((item) => item.split("=")[1])[0];
+  }
+
+  requestURLParamArray.filter((item) => {
+    item.match(/u[0-9]{1,}=.*/g) && customFloodlightVariable.push(item);
+  });
+
+  floodlightDetails["cfv"] = customFloodlightVariable;
+  floodlightDetails["countingMethod"] = defineCountingMethod(floodlightDetails);
+
+  return floodlightDetails;
+}
+
+function defineCountingMethod(floodlightInfoList) {
+  if (floodlightInfoList.ord && !floodlightInfoList.num && floodlightInfoList.qty !== undefined && floodlightInfoList.cost !== undefined) {
+    return "Transactions / Items Sold";
+  } else if (floodlightInfoList.ord == "1" && floodlightInfoList.num && !floodlightInfoList.qty && !floodlightInfoList.cost) {
+    return "Unique";
+  } else if (floodlightInfoList.ord && !floodlightInfoList.num && !floodlightInfoList.qty && !floodlightInfoList.cost) {
+    return "Standard / Session";
+  } else {
+    return "Unable to define a correct counting method";
+  }
+}
+
+function renderCustomFloodlightVariable(floodlightInfoList) {
+  if (floodlightInfoList.cfv.length == 0) {
+    return "<p>- No custom floodlight variable -</p>";
+  }
+
+  let cfvLayout = "";
+
+  for (let variable of floodlightInfoList.cfv) {
+    cfvLayout += `<li class="row">
+      <p class="col-6">${variable.split("=")[0]}</p>
+      <p class="col-6">${decodeURIComponent(variable.split("=")[1])}</p>
+    </li>`;
+  }
+
+  let finalCFVLayout = "<ul>" + cfvLayout + "</ul>";
+
+  return finalCFVLayout;
+}
+
+const floodlightInfoList = generateFloodlightInfoList(standardRequestURL);
+
+// console.log(generateFloodlightInfoList(itemSoldRequestURL));
+
+console.log(renderCustomFloodlightVariable(floodlightInfoList));
